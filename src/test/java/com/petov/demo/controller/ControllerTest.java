6c339e8fd4481b84c77b971c6cf50e01d661bda9
Dmitry Petrov <dmitrypetrov93gmail.com>
2020-05-24 23:56:08 +0300

    add: fail test

--- src/test/java/com/petov/demo/controller/ControllerTest.java
+++ src/test/java/com/petov/demo/controller/ControllerTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.Assert;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -25,7 +26,15 @@ class ControllerTest {
         this.mockMvc.perform(get("/"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().string(containsString("")));
+                .andExpect(content().string(containsString("Hello")));
+    }
+
+    @Test
+    public void failTest() throws Exception {
+        this.mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("fail test")));
     }
 
 }
